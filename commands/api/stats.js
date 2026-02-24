287fbb62d822733052c32720e655983c8b18dfb4
Leissom <leisoom.gg@gmail.com>
2026-02-23 23:38:08 -0800

    Convert bal command to stats command

--- commands/api/stats.js
+++ commands/api/stats.js
@@ -1,12 +1,12 @@
 const api = require('../../src/utils/api');
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const axios = require('axios');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('bal')
-        .setDescription('fetch balance for the user')
-	    .addStringOption((option) => option.setName('user').setDescription('The user to check bal of').setRequired(true)),
+        .setName('stats')
+        .setDescription('fetch statistics for the user')
+	    .addStringOption((option) => option.setName('user').setDescription('The user to check stats of').setRequired(true)),
 
     async execute(interaction) {
         const user = interaction.options.getString('user')
@@ -24,7 +24,26 @@ module.exports = {
                 currency: 'USD',
             }).format(money);
 
-            await interaction.reply(`${user}'s money: ${formatted}`);
+            const embed = new EmbedBuilder()
+                .setTitle(user)
+                .setDescription(`${user}'s stats`)
+                .setColor(0x5865F2)
+                .addFields(
+                    { name: 'Blocks Broken', value: response.data.result.broken_blocks, inline: false },
+                    { name: 'Deaths', value: response.data.result.deaths, inline: false },
+                    { name: 'Kills', value: response.data.result.kills, inline: false },
+                    { name: 'Mobs Killed', value: response.data.result.mobs_killed, inline: false },
+                    { name: 'Money', value: formatted, inline: false },
+                    { name: 'Money Made From Sell', value: response.data.result.money_made_from_sell, inline: false },
+                    { name: 'Money Spent on Shop', value: response.data.result.money_spent_on_shop, inline: false },
+                    { name: 'Placed_Blocks', value: response.data.result.placed_blocks, inline: false },
+                    { name: 'Playtime', value: response.data.result.playtime, inline: false },
+                    { name: 'Shards', value: response.data.result.shards, inline: false },
+                )
+                .setFooter({ text: 'Requested via slash command' })
+                .setTimestamp();
+
+            await interaction.reply({ embeds: [embed] });
 
         } catch (error) {
             console.error(error);
